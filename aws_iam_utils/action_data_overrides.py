1f6f02a9748aeaa246004f4174a8bb45beff5a60
jtyers <40065647+jtyers@users.noreply.github.com>
2022-06-09 14:32:19 +0100

    fix: add createfunctionurlconfig override

--- aws_iam_utils/action_data_overrides.py
+++ aws_iam_utils/action_data_overrides.py
@@ -5,6 +5,10 @@ This is a mapping of actions to overridden action data. This is primarily used t
 """
 ACTION_DATA_OVERRIDES = {
   # NB: keys must be all lowercase
+  'lambda:createfunctionurlconfig': {
+    'action': 'lambda:createfunctionurlconfig',
+    'access_level': WRITE,
+  },
   'lambda:getfunctionurlconfig': {
     'action': 'lambda:getfunctionurlconfig',
     'access_level': READ,
