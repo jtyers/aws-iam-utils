76c21180981449304a3a82738ace3f7a5f041746
jtyers <40065647+jtyers@users.noreply.github.com>
2022-06-13 14:13:13 +0100

    release: 1.7.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="aws-iam-utils",
-    version="1.6.0",
+    version="1.7.0",
     description="AWS IAM utility library",
     long_description=README,
     long_description_content_type="text/markdown",
